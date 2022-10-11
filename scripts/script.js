264803dd0a0c1f551b9348d30e52e08167cc4fbe
Liubomyr Olianych <lubomir.olianich@gmail.com>
2022-10-11 22:40:25 +0300

    main elements

---
+++ scripts/script.js
@@ -0,0 +1,4 @@
+const btns = document.querySelectorAll(".btn");
+const confirmation = document.querySelector(".modal");
+// const form = document.querySelector(".emailForm");
+// let email = document.querySelector("email");
