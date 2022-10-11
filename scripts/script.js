24bc5d1d4d7449388b4e1c2cb026bae7c19fde7c
Liubomyr Olianych <lubomir.olianich@gmail.com>
2022-10-12 00:46:17 +0300

    final result

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,33 @@
 const btns = document.querySelectorAll(".btn");
 const confirmation = document.querySelector(".modal");
-// const form = document.querySelector(".emailForm");
-// let email = document.querySelector("email");
+const form = document.querySelector(".emailForm");
+const header = document.querySelector(".header");
+const result = document.querySelector(".result");
+let email = "";
+
+document.querySelector(".emailForm").addEventListener("submit", (e) => {
+  e.preventDefault();
+  email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+
+  if (password !== 0 && email.includes("@")) {
+    confirmation.classList.add("modalOn");
+    let span = document.querySelector(".emailToConfirm");
+    span.innerHTML = email;
+  }
+});
+
+document.querySelector(".cancel").addEventListener("click", (e) => {
+  e.preventDefault();
+  confirmation.classList.remove("modalOn");
+});
+
+document.querySelector(".confirm").addEventListener("click", (e) => {
+  e.preventDefault();
+  let span = document.querySelector(".emailConfirmed");
+  span.innerHTML = email;
+  form.classList.add("hide");
+  header.classList.add("hide");
+  confirmation.classList.remove("modalOn");
+  result.classList.remove("hide");
+});
